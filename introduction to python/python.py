bc11474488b48bc25e5a6c5c9c06a9f3ac7db870
shivam jha <shivamjha7622@gmail.com>
2025-10-19 13:43:30 +0530

    introduction to python

---
+++ introduction to python/python.py
@@ -0,0 +1,36 @@
+# What is python
+    # python is a high-level programming language used to tell computers what's to do. it's know for being:
+         # simple to read and write
+         # versatile (used in web development, data science, automation, AI and many more)
+         # Beginner-friendly, yet powerful enough for professionals.
+
+# it was created by Guido  Van Rossum and first released in  1991.
+
+# Why learn python?
+    # Easy syntex : Looks almost like  english.
+    # Huge library support : Tools for everything from math to machine learning.
+    # Massive community : Tons of tutorials, forums and help.
+    # Career opportunities : used by companies like Google, Netflix and Nasa also.
+
+
+# How python works?
+    # you write python code in a file or editor, and the python interpreter reads and runs it line by line.
+# Example
+print("Hello, Shivam!")
+# This tells python to display the message : Hello, Shivam!
+
+# How to install python
+     # 1. Go to python.org/downloads
+     # 2. Download the latest version
+     # 3. Run the installer and check "Add python to path"
+     # 4. Click install now
+     # 5. Open Commant prompt and type : python --version
+            # you should see something like python3.11.5
+
+# Congratulations! You have successfully installed python.
+
+# Try your first python program
+    # Open a text editor and type the example code above.
+    # Save the file as hello.py
+    # Open command prompt, navigate to the file location and type : python hello.py
+    # You should see the output : Hello, Shivam!
