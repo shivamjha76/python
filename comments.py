50a104fa3954b5755c227c3c16e1dca4daccb7b9
shivam jha <shivamjha7622@gmail.com>
2025-10-19 13:52:53 +0530

    comments in python

---
+++ comments.py
@@ -0,0 +1,31 @@
+#------------------------What Are Comments------------------------------?
+# Comments are notes you write in your code that Python completely ignores when running the program. They’re meant for you and other humans to understand what the code is doing.
+# Think of comments like sticky notes in a notebook—they help explain things but don’t affect the actual work.
+
+# ------------------------Why Use Comments------------------------------?
+           # To explain what your code does
+           # To make your code easier to read
+           # To leave reminders
+           # To temporarily disable parts of code while testing
+
+# ------------------------How to Write Comments in Python? ----------------------- #
+
+# ---------------------Single-Line Comments -------------------------- #
+          # Use the (#) symbol to write a comment:
+          # This prints a greeting print("Hello, Shivam!")
+          # Everything after the # is ignored by Python.
+          # You can also write comments at the end of a line:
+print("Welcome!") # This is a welcome message
+
+# -----------------------Multi-Line Comments ------------------------- #
+          # Python doesn’t have a special multi-line comment symbol, but you can use one of these two methods:
+                        # 1. Multiple # lines:
+                              # This is a multi-line comment # explaining what the code does # line by line print("Hello again!")
+                        # 2. Triple quotes (''' or """):
+''' This is a multi-line comment using triple quotes. Python ignores this if it's not assigned to a variable. '''
+print("Python is fun!")
+
+# --------------------- Commenting Out Code ---------------------- #
+        # You can use comments to disable code without deleting it:
+        # print("This line won't run") print("Only this line runs")
+        # This is super useful when debugging or testing.
